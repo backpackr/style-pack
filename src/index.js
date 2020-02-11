5d670ff6580e18165810dddc4807a0103e0e2c8d
Seungjae Jun <ninanung0503@gmail.com>
2020-02-11 18:43:47 +0900

    flex_grid, color, container관련 class수정

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 require('./button.scss');
 require('./container.scss');
+require('./flex_grid.scss');
 require('./grid.scss');
+require('./color.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 use: [
                     'style-loader',
                     'css-loader',
+                    'postcss-loader',
                     'sass-loader'
                 ]
             }
